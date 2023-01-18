62a8f39dcb5cad541e0f5cad73be2c6e8c569a23
scott-hall7 <scotthall179@gmail.com>
2023-01-18 09:38:28 -0600

    adding analyze array script

--- scripts/analyzeArray.js
+++ scripts/analyzeArray.js
@@ -1,4 +1,36 @@
 function analyzeArray(array)  {
+    let object = {};
+    
+    const average = (array) =>   {
+        let total = 0;
+        for(let i = 0; i < array.length; i++)   {
+            total += array[i]
+        }
+        return total / array.length
+    }
+
+    const min = (array) =>   {
+        let sortedArray = array.sort((a, b) => a - b);
+        return sortedArray[0];
+    }
+
+    const max = (array) =>   {
+        let sortedArray = array.sort((a, b) => a - b);
+        return sortedArray[array.length - 1];
+    }
+
+    const length = (array) =>   {
+        return array.length;
+    }
+
+    
+    object.average = average(array);
+    object.min = min(array);
+    object.max = max(array);
+    object.length = length(array);
+
+    return object;
 
 }
+console.log(analyzeArray([1,8,3,4,2,6]))
 //export { analyzeArray };
